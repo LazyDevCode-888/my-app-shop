b905ce40bce56c84ca3366e807edd6350dfa9954
lazydevcode <arthitj77877@gmail.com>
2025-05-21 20:14:09 +0700

    go

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-      dark: "rgb(17 24 39)", // หรือจะใช้ rgba ก็ได้
-    },
+        dark: "rgb(17 24 39)",
+      },
       fontFamily: {
-        sans: ['Kanit', 'sans-serif'], // ฟอนต์หลัก
-        orbitron: ['Orbitron', 'sans-serif'], // สำหรับโลโก้หรือหัวเรื่อง
+        sans: ["Kanit", "sans-serif"],
+        orbitron: ["Orbitron", "sans-serif"],
       },
     },
   },
   plugins: [require("daisyui")],
-}
+};
